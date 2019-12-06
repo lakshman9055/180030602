8bc2c6a6c3e49ef578053e70ec6e6f539d0b564a
lakshman9055 <58474350+lakshman9055@users.noreply.github.com>
2019-12-06 17:14:33 +0530

    Flow Control

---
+++ p21.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class P21
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
+}}
+
